75d227f385fa8c35e33e1c48d5a510c27e54a595
A.Popov <angelpopov@yahoo.com>
2019-06-12 13:53:09 +0300

    Sending messages to each other and checks for it

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # omni-check-selenium
 
-Check availability of reference app
+Check availability of reference app using selenium
+## Assumptions
+ - Selenium webdriver is running (java -jar selenium-standalone...)
 
-Config parameters should be in src/Config.hs. Config.hs is based on Config.hs.template
+ - Config parameters should be in src/Config.hs. Config.hs is based on Config.hs.template
+
+## Scenario
+Open ump page
+Login u1 and u2
+sends message to each other
+checks that message has been received
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,45 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Lib ( checkOmni ) where
+import Data.Time
+import Data.Text
+import Test.WebDriver 
+import Test.WebDriver.Session
+import Test.WebDriver.Commands
+import Test.WebDriver.Commands.Wait
+import Test.WebDriver.JSON (ignoreReturn)
+import qualified Config as C
+import Control.Concurrent.Async
+type User = Text
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+chromeConfig :: WDConfig
+chromeConfig = useBrowser chr defaultConfig
+  { wdHost = "0.0.0.0", wdPort = 4444, wdHTTPRetryCount = 50 }
+  where chr = chrome
+
+session = runSession chromeConfig $ getSession
+  
+loginUMP :: WD ()
+loginUMP = do
+  (openPage "https://nexmo-omni-channel.geeks-lab.net/cs/ump")
+  (findElem (ById "apiSecret")) >>= sendKeys C.apiKey
+  (findElem (ById "apiKey")) >>= sendKeys C.apiSecret
+  (findElem (ByXPath "//button[contains(text(),Login)]")) >>= click
+  (findElem (ByXPath "//span[contains(text(),'Your applications')]")) >>= click
+  elem <- (waitUntil 5 (findElem $ ByXPath $ "//span[contains(text(),'" <> C.appId <> "')]"))
+  return ()
+
+
+sendMessage :: User -> Text -> User -> WD()
+sendMessage user msg toUser = do
+  (openPage "https://nexmo-omni-channel.geeks-lab.net/cs/app")
+  (findElem (ById "username")) >>= sendKeys user
+  (findElem (ById "password")) >>= sendKeys C.globalPassword
+  (findElem (ById "appId")) >>= sendKeys C.appId
+  (findElem (ByXPath "//button[contains(text(),Login)]")) >>= click
+  (waitUntil 10 (findElem (ByXPath "//input[contains(@placeholder,'Type a message')]"))) >>= sendKeys (msg <>"from " <> user <> "\n")
+  (waitUntil 10 (findElem (ByXPath ("//b[contains(text(),'" <> (msg <>"from " <> toUser) <>"')]"::Text)))) >>= click
+  
+checkOmni :: IO()
+checkOmni = do
+  time <- (pack.show)<$> getCurrentTime
+  mapConcurrently_ (\wd -> session >>= flip runWD wd) [loginUMP,sendMessage "u1" time "u2" ,sendMessage "u2" time "u1"]
